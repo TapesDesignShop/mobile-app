665db2f3c9d1cda13ec9bc8264e52b684a68652c
SuparnoD <suparnodeb@hotmail.co.uk>
2022-04-12 13:32:55 +0100

    services

--- screens/Services.js
+++ screens/Services.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
+import Carousel from "react-native-snap-carousel";
+import React, { useState, useEffect, useRef } from "react";
 
 import Header from "../components/Header";
 import { Colors } from "../constant/Colors";
@@ -8,6 +9,67 @@ import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 
 const Services = ({ navigation }) => {
+  const { width: screenWidth } = Dimensions.get("window");
+
+  const ITEMS = [
+    {
+      title: "PRINTING",
+      text: "Printing can be completed on the premises. Email your presentation/report or dissertation/thesis to us and we will give you a quote.",
+      img: require("../assets/print.jpg")
+    },
+    {
+      title: "BINDING",
+      text: "Binding can be completed in A4 or A3 sizes. Wire binding can normally be completed on the same day. Hardback binding with gold letters must be booked in advance.",
+      img: require("../assets/binding.jpg")
+    },
+  ];
+
+  const [entries, setEntries] = useState([]);
+  const carouselRef = useRef(null);
+
+  const goForward = () => {
+    carouselRef.current.snapToNext();
+  };
+
+  useEffect(() => {
+    setEntries(ITEMS);
+  }, []);
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.serviceContainer}>
+        <View style={styles.serviceImg}>
+          <Image
+            style={{ height: "100%", width: "100%", resizeMode: "cover", borderRadius: 20}}
+            source={item.img}
+          />
+        </View>
+        <View style={styles.serviceItem}>
+          <Text
+            style={{
+              fontFamily: "mont-extra-bold",
+              fontSize: 18,
+              color: Colors.honeydew,
+              textAlign: "center",
+              opacity: 100,
+            }}
+          >
+            {item.title}
+          </Text>
+          <Text
+            style={{
+              fontFamily: "roboto-regular",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            {item.text}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Header />
@@ -41,10 +103,15 @@ const Services = ({ navigation }) => {
         colors={["white", Colors.paradisePink]}
         style={styles.bottomContainer}
       >
-        <View style={{...styles.serviceContainer, backgroundColor: Colors.blueViolet}}>
-          <View
-            style={styles.serviceItem}></View>
-        </View>
+        <Carousel
+          layout={"default"}
+          ref={carouselRef}
+          sliderWidth={screenWidth}
+          sliderHeight={screenWidth}
+          itemWidth={screenWidth - 60}
+          data={entries}
+          renderItem={renderItem}
+        />
       </LinearGradient>
       <Footer />
     </View>
@@ -73,14 +140,28 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height > 700 ? "52.5%" : "55%",
   },
   serviceContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     flexDirection: "row",
-    height: "60%",
+    height: Dimensions.get("window").height > 700 ? "80%" : "79%",
     width: "95%",
     borderRadius: 10,
-    opacity: 0.5,
-    bottom: Dimensions.get("window").height > 700 ? 10 : 40
+    backgroundColor: "rgba(84,13,110,0.3)",
+    top: 10,
   },
-  serviceItem: {},
+  serviceItem: {
+    marginTop: "auto",
+    padding: 6,
+    bottom: Dimensions.get("window").height > 700 ? 10 : 0,
+  },
+  serviceImg: {
+    backgroundColor: "transparent",
+    height: Dimensions.get("window").height > 700 ? "60%" : "65%",
+    width: Dimensions.get("window").height > 700 ? "95%" : "95%",
+    bottom: Dimensions.get("window").height > 700 ? 50 : 60,
+    position: "absolute",
+    top: Dimensions.get("window").height > 700 ? 5 : 5,
+  },
 });
 
 export default Services;
